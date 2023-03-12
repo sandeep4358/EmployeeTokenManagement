a311970ce1dfe8c0a83e935bf8ae9e56a8865d1d
sandeep4358 <sandeep4358@gmail.com>
2023-03-12 13:33:33 +0530

    Initial commit

---
+++ src/main/java/com/javaFruit/EmployeeTokenManagement/EmployeeTokenManagementApplication.java
@@ -0,0 +1,13 @@
+package com.javaFruit.EmployeeTokenManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTokenManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTokenManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaFruit/EmployeeTokenManagement/EmployeeTokenManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaFruit.EmployeeTokenManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTokenManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
